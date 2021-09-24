93d1d20bcf389675836fa20b36a376caa5791fce
Akarsh Barar <barar.akarsh8@gmail.com>
2021-09-25 02:25:24 +0530

    Added Routes and Some custom CSS for Home Page. Added Error page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect}  from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Pages/Home';
+import Error from './Pages/Error';
+
 
 function App() {
+  const [title, setTitle] = useState("POS Financing Soutions");
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>        
+        <Switch>
+          <Route exact path="/" component = {Home}></Route>
+          <Route path="*" component = {Error}></Route>
+  
+        </Switch>
+    </Router>
     </div>
   );
 }
